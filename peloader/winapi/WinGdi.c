a23ebaee381c2f7ae2e752121e091051062b53be
HeapRaid <79619458+HeapRaid@users.noreply.github.com>
2021-03-06 18:34:32 -0800

    WinGdi: Add DeleteObject() stub.

---
+++ peloader/winapi/WinGdi.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <search.h>
+#include <assert.h>
+
+#include "winnt_types.h"
+#include "pe_linker.h"
+#include "ntoskernel.h"
+#include "log.h"
+#include "winexports.h"
+#include "util.h"
+#include "strings.h"
+
+BOOL DeleteObject(HANDLE ho)
+{
+    DebugLog("");
+    return FALSE;
+}
+
+DECLARE_CRT_EXPORT("DeleteObject", DeleteObject);
